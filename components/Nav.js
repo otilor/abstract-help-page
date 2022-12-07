0b80041bf8ab653a8e35924c35ddaba150554121
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2022-12-07 14:09:34 +0100

    remove border

--- components/Nav.js
+++ components/Nav.js
@@ -8,9 +8,9 @@ const navigation = [
 
 export default function Nav() {
     return (
-        <header className="bg-indigo-600">
+        <header className="bg-black-600">
             <nav className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8" aria-label="Top">
-                <div className="w-full py-6 flex items-center justify-between border-b border-indigo-500 lg:border-none">
+                <div className="w-full py-6 flex items-center justify-between">
                     <div className="flex items-center">
                         <a href="#">
                             <span className="sr-only">Workflow</span>
