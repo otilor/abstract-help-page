40ab27cd49812df0cfc7be9a2934485f1a9b3c1e
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2022-12-07 15:06:00 +0100

    close enough

--- components/Nav.js
+++ components/Nav.js
@@ -2,7 +2,7 @@ export default function Nav() {
     return (
         <header className="bg-black">
             <nav className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8" aria-label="Top">
-                <div className="w-full py-6 flex items-center justify-between">
+                <div className="w-full py-4 flex items-center justify-between">
                     <div className="flex items-center">
                         <a href="#">
                             <span className="sr-only">Workflow</span>
