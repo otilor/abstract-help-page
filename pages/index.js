f07b5398b2ac8cd95e071114b627142cd7f7a721
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2022-12-07 15:04:33 +0100

    remove footer

--- pages/index.js
+++ pages/index.js
@@ -17,18 +17,6 @@ export default function Home() {
         <h3 className='text-7xl mt-24'>How can we help?</h3>
         <input className='p-5 w-4/6 border border-black rounded sm:auto mb-24' placeholder='Search'/>
       </div>
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
   )
 }
