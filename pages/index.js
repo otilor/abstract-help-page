2d135ba0461aa7fa88b7964a9c98b1a01e2ba586
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2022-12-08 05:37:08 +0100

    add feature

--- pages/index.js
+++ pages/index.js
@@ -38,7 +38,7 @@ export default function Home() {
       <div class="bg-white py-24 sm:py-32 lg:py-40">
         <div class="mx-auto max-w-7xl px-6 lg:px-8">
 
-          <div class="max-w-lg sm:mx-auto md:max-w-none">
+          <div class="max-w-lg sm:mx-20 md:max-w-none">
             <div class="grid grid-cols-1 gap-y-16 md:grid-cols-2 md:gap-x-12 md:gap-y-16">
               <div class="relative flex flex-col gap-6 sm:flex-row md:flex-col lg:flex-row">
                 <div class="flex h-12 w-12 items-center justify-center rounded-xl bg-indigo-500 text-white sm:shrink-0">
