1d82467b0275eb97957223d63ddbb0bb0cdb4b2c
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2022-12-08 13:06:44 +0100

    add black background

--- components/Footer.js
+++ components/Footer.js
@@ -91,7 +91,7 @@ const navigation = {
   
   export default function Footer() {
     return (
-      <footer className="bg-white" aria-labelledby="footer-heading">
+      <footer className="bg-black" aria-labelledby="footer-heading">
         <h2 id="footer-heading" className="sr-only">
           Footer
         </h2>
