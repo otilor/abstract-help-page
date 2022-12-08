bffb00011104269d8ecce6e6bb0df46bb2ffd6cb
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2022-12-08 05:34:01 +0100

    removed text

--- pages/index.js
+++ pages/index.js
@@ -37,20 +37,8 @@ export default function Home() {
 
       <div class="bg-white py-24 sm:py-32 lg:py-40">
         <div class="mx-auto max-w-7xl px-6 lg:px-8">
-          <div class="sm:text-center">
-            <h2 class="text-lg font-semibold leading-8 text-indigo-600">
-              Transactions
-            </h2>
-            <p class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-              A better way to send money
-            </p>
-            <p class="mx-auto mt-6 max-w-2xl text-lg leading-8 text-gray-600">
-              Lorem ipsum dolor sit amet consect adipisicing elit. Possimus
-              magnam voluptatum cupiditate veritatis in accusamus quisquam.
-            </p>
-          </div>
 
-          <div class="mt-20 max-w-lg sm:mx-auto md:max-w-none">
+          <div class="max-w-lg sm:mx-auto md:max-w-none">
             <div class="grid grid-cols-1 gap-y-16 md:grid-cols-2 md:gap-x-12 md:gap-y-16">
               <div class="relative flex flex-col gap-6 sm:flex-row md:flex-col lg:flex-row">
                 <div class="flex h-12 w-12 items-center justify-center rounded-xl bg-indigo-500 text-white sm:shrink-0">
